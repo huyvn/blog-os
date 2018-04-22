03a40a84eb6a6fd6f04a8c1390d7a4a4ef13b73a
Huy Nguyen <huyvn.steven@gmail.com>
2018-04-22 11:30:54 +0800

    Added specs to build kernel which prints Hello World in VGA, and to create bootable images

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,28 @@
 
 #[lang = "panic_fmt"] // define a function to be called on system panic
 #[no_mangle] 
-pub extern "C" fn rust_begin_panic(_msg: core::fmt::Arguments,
-    _file: &'static str, _line: u32, _column: u32) -> !
-{
+pub extern "C" fn rust_begin_panic(
+    _msg: core::fmt::Arguments,
+    _file: &'static str,
+    _line: u32,
+    _column: u32
+) -> ! {
     loop {}
 }
+
+static HELLO: &[u8] = b"Hello World!";
 
 // disable function name mangle/hashing
 // because linker expects function name `_start` by default
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *const u8 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate(){
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
